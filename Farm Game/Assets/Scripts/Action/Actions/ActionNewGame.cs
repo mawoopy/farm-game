227bd7167d0a3430f1e9e2fbc4a9c746dc74b08d
goop <goop>
2026-06-18 14:40:43 +0200

    imported RPG Farming Kit

---
+++ Farm Game/Assets/Scripts/Action/Actions/ActionNewGame.cs
@@ -0,0 +1,23 @@
+using Lowscope.Saving;
+using Lowscope.Saving.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Actions/New Game" )]
+public class ActionNewGame : ScriptableObject
+{
+    [SerializeField]
+    private StringVariable firstScene;
+
+    [SerializeField]
+    private StringVariable playerScene;
+
+    public void Execute()
+    {
+        int getUnusedSlot = SaveFileUtility.GetAvailableSaveSlot();
+        SaveMaster.SetSlot(getUnusedSlot, true);
+
+        SceneManager.LoadScene(firstScene.Value);
+        SceneManager.LoadScene(playerScene.Value, LoadSceneMode.Additive);
+    }
+}
---
+++ Farm Game/Assets/Scripts/Action/Actions/ActionOpenScene.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Actions/Open New Scene")]
+public class ActionOpenScene : ScriptableObject
+{
+    [SerializeField]
+    private string sceneName;
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+        /*
+        for (int i = SceneManager.sceneCount - 1; i >= 0; i--)
+        {
+            Scene getScene = SceneManager.GetSceneAt(i);
+
+            if (getScene != SceneManager.GetActiveScene())
+            {
+                SceneManager.UnloadSceneAsync(getScene.buildIndex);
+            }
+        }
+        */
+
+        SceneManager.LoadScene(sceneName);
+
+        Resources.UnloadUnusedAssets();
+    }
+}
